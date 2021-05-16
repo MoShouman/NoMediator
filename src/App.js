e783492cdf283ac21a522e3692c22bf21ad83a38
MuEbrahim <muhammedebrahim00@gmail.com>
2021-05-16 17:17:04 +0200

    User Stack

--- src/App.js
+++ src/App.js
@@ -1,25 +1,197 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import {Switch, Route} from 'react-router'
+import {Redirect} from 'react-router-dom'
+import NavBar from './Components/NavBar'
+import Login from './Components/Auth/Login'
+import Register from './Components/Auth/Register'
+import Profile from './Components/Profile'
+import ChangeInfo from './Components/ChangeInfo'
+import Default from './Components/Default'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {  
+  state = {
+    currentUserInfo:{},
+    isAuthenticated:false
+  }
+
+  //Change Informations
+  changeInfo = (data, props) => {
+    let status = 0
+    const username = data.username 
+    const email = data.email 
+    const password = data.password
+    const city = data.city 
+    const town = data.town
+    const type = data.type
+    const logo = data.logo
+    const id = data.id
+
+    console.log(data)
+    fetch(`https://your-products-manager.herokuapp.com/api/user/edit/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username,email,password, city, town, type, logo})
+    
+    }).then(response => {status = response.status; return response.json()})
+      .then(data => {
+        if(status === 200){
+          this.setState({currentUserInfo:data,isAuthenticated:true});
+          toast.success('your information has been successfully changed', {
+            position: "top-center",
+            autoClose: 4000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            });
+          props.history.replace('/profile');
+
+        }
+        else{
+          console.log(data.message)
+          toast.error(data.message, {
+          position: "top-center",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
+        }
+      })  
+  }
+
+  //register function
+  registerHandler = (data,props) =>{
+    let status = 0
+    const username = data.username 
+    const email = data.email 
+    const password = data.password
+
+    fetch('https://your-products-manager.herokuapp.com/api/user/register', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username,email,password})
+    
+    }).then(response => {status = response.status; return response.json()})
+      .then(data => {
+        if(status === 200){
+          this.setState({currentUserInfo:data,isAuthenticated:true});
+          toast.success('registered', {
+            position: "top-center",
+            autoClose: 4000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            });
+            props.history.replace('/');
+        }
+        else{
+          console.log(data.message)
+          toast.error(data.message, {
+          position: "top-center",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
+        }
+      })   
+  }
+
+  //login function
+  loginHandler = (data,props) =>{
+    let status = 0
+    fetch('https://your-products-manager.herokuapp.com/api/user/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(response => {status = response.status; return response.json()})
+      .then(data => {
+        if(status === 200){
+          this.setState({currentUserInfo:data,isAuthenticated:true});
+          toast.success('logged in', {
+            position: "top-center",
+            autoClose: 4000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            });
+            props.history.replace('/');
+        }
+        else{
+          console.log(data.message)
+          toast.error(data.message, {
+          position: "top-center",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
+        }
+      })   
+  }
+
+  //sign out function
+  signOutHandler = () => {
+    this.setState({currentUserInfo:{},isAuthenticated:false});
+  }
+  
+
+  render(){
+  
+    const isAuthenticated = this.state.isAuthenticated;
+    return (
+      <div className="App">
+        <NavBar currentUserInfo = {this.state.currentUserInfo} 
+                  isAuthenticated = {this.state.isAuthenticated}
+                  signOutHandler = {this.signOutHandler}
+          />
+        <ToastContainer/>
+        <Switch> 
+             <Route path='/register' render={ props =>{
+                return isAuthenticated ?  <Redirect to="/" /> : <Register registerHandler = {this.registerHandler} {...props}/>;
+              }}>
+              </Route>
+              <Route path='/login' render={ props => {
+                return isAuthenticated ?  <Redirect to="/" /> : <Login loginHandler = {this.loginHandler} {...props}/>;
+              }}>
+              </Route>
+              <Route path='/Profile' render={ props =>{
+                return !isAuthenticated ?  <Redirect to="/" /> : <Profile userInfo = {this.state.currentUserInfo} changeInfo={this.changeInfo} {...props}/>;
+              }}>
+              </Route>
+              <Route path='/changeInfo' render={ props =>{
+                return !isAuthenticated ?  <Redirect to="/" /> : <ChangeInfo userInfo = {this.state.currentUserInfo} changeInfo={this.changeInfo} {...props}/>;
+              }}>
+              </Route>
+              <Route component={Default}/>
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Components/ChangeInfo.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+import InputComponent from '../constant/InputComponent'
+
+class ChangeInfo extends Component {
+    
+    state = {
+        disabled:false,
+        email:this.props.userInfo.email,
+        username:this.props.userInfo.username,
+        city:this.props.userInfo.city,
+        town:this.props.userInfo.town,
+        type:this.props.userInfo.type,
+        password:this.props.userInfo.password,
+        id:this.props.userInfo._id
+    }
+
+    onChangeHandler =  (event) => {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    onSubmitHandler = () => {
+        this.props.changeInfo({...this.state}, this.props)
+    }
+
+    render() { 
+        return ( 
+            <div className='container'>
+            <h2 className='text-uppercase d-block text-center my-4'>Edit Informations</h2>
+            <div className='row '>
+                <div className='col-sm-5 col-md-12 d-flex justify-content-center'>
+                    <img style={{borderRadius:'50%', maxWidth:'70px', maxHeight:'70px'}} src={this.props.userInfo.logo} alt='image'/>                         
+                </div>
+                <form onSubmit = {e => {e.preventDefault(); this.onSubmitHandler()}}  className='d-block col-5 mx-auto shadow p-3 mb-5 mt-1 rounded'>
+                    <InputComponent title = 'Username' 
+                                     name = 'username'
+                                     type = 'text'     
+                                     disabled={this.state.disabled}
+                                     value = {this.state.username}
+                                     required={true}
+                                     onChangeHandler = {this.onChangeHandler}                      
+                                    />
+                     <InputComponent title = 'Email'
+                                      name = 'email' 
+                                      type = 'text'
+                                      disabled={this.state.disabled}
+                                      value = {this.state.email}
+                                      required={true}
+                                      onChangeHandler = {this.onChangeHandler}              
+
+                                    />
+                     <InputComponent title = 'City'
+                                     name = 'city'
+                                     type = 'text'
+                                     disabled={this.state.disabled}
+                                     value = {this.state.city}
+                                     required={false}
+                                     onChangeHandler = {this.onChangeHandler}              
+
+                                    />
+                     <InputComponent title = 'Town'
+                                     name = 'Town'
+                                     type = 'text'
+                                     disabled={this.state.disabled}
+                                     value = {this.state.city}
+                                     required={false}
+                                     onChangeHandler = {this.onChangeHandler}              
+
+                                    />
+                     <InputComponent title = 'Type'
+                                     name = 'type'
+                                     type = 'text'
+                                     disabled={this.state.disabled}
+                                     value = {this.state.type}
+                                     required={false}
+                                     onChangeHandler = {this.onChangeHandler}              
+                                    />
+                    <button  type='submit' className='bt btn-dark px-4 py-2 rounded-lg col-sm-5 col-md-12 d-block'> Save Changes</button> 
+                </form>
+            </div>
+        </div>
+        );
+    }
+}
+ 
+export default ChangeInfo;
